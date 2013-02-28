3f508cbdf75bf3e96b50aef988a616c231414fe9
elarex <jeanlrx@gmail.com>
2013-02-28 10:43:00 +0200

    re-enable recipies and bump version

--- lib/capistrano_recipes.rb
+++ lib/capistrano_recipes.rb
@@ -1,13 +1,4 @@
 require 'capistrano'
 require 'capistrano/cli'
 
-Capistrano::Configuration.instance.load do
-  namespace :foo do
-    desc "Testing the gem"
-    task :bar do
-      run "#{sudo} echo 'this is a test'"
-    end
-  end
-end if Capistrano::Configuration.instance
-
-#Dir.glob(File.join(File.dirname(__FILE__), '/recipes/*.rb')).sort.each { |f| load f }
+Dir.glob(File.join(File.dirname(__FILE__), '/recipes/*.rb')).sort.each { |f| load f }
