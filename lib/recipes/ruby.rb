2350991fc03fa2492c07a90295378776e8dc826f
elarex <jeanlrx@gmail.com>
2013-09-30 14:49:50 +0200

    deprecate 1.9.3 and add in 2.0 ruby installation

--- lib/recipes/ruby.rb
+++ lib/recipes/ruby.rb
@@ -1,10 +1,20 @@
 Capistrano::Configuration.instance.load do
   namespace :ruby do
-    desc "Install Ruby, and the Bundler gem"
-    task :install, roles: :app do
+    desc "Install Ruby 1.9.3, and the Bundler gem"
+    task :install_193, roles: :app do
       run "#{sudo} add-apt-repository -y ppa:brightbox/ruby-ng"
       run "#{sudo} apt-get -y update"
       run "#{sudo} apt-get -y install ruby1.9.3"
+
+      run "#{sudo} gem install bundler --no-ri --no-rdoc"
+    end
+    after "deploy:install", "ruby:install"
+
+    desc "Install Ruby 2.0, and the Bundler gem"
+    task :install, roles: :app do
+      run "#{sudo} add-apt-repository -y ppa:brightbox/ruby-ng"
+      run "#{sudo} apt-get -y update"
+      run "#{sudo} apt-get -y install ruby2.0"
 
       run "#{sudo} gem install bundler --no-ri --no-rdoc"
     end
--- lib/version.rb
+++ lib/version.rb
@@ -1,3 +1,3 @@
 module CapistranoRecipes
-  VERSION = "0.9.3"
+  VERSION = "0.9.4"
 end
